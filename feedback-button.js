aef43f85989dc28e89847bb971bdf5cd82b3ab94
Jensrisc <jens.rischmueller@gmail.com>
2025-07-23 10:39:36 +0200

    Update feedback-button.js

--- feedback-button.js
+++ feedback-button.js
@@ -1,9 +1,24 @@
-window.FeedbackButton = {
-    onClick: () => {
-        const loginBase = 'https://iad.merlon.amazon.dev/console?awsAccountId=089910738259&awsPartition=aws&accountName=EURME-PredictiveAnalytics&sessionDuration=43200&iamRole=arn:aws:iam::089910738259:role/S3_Ranger_Tech';
-        const currentHost = window.location.host;
-        const targetUrl = `https://${currentHost}/s3/upload/ranger-production?region=eu-west-1&bucketType=general&prefix=feedback/`;
-        const finalUrl = `${loginBase}&redirect_uri=${encodeURIComponent(targetUrl)}`;
-        window.open(finalUrl, '_blank');
-    }
-};
+(function () {
+    'use strict';
+
+    const loginBase = 'https://iad.merlon.amazon.dev/console?awsAccountId=089910738259&awsPartition=aws&accountName=EURME-PredictiveAnalytics&sessionDuration=43200&iamRole=arn:aws:iam::089910738259:role/S3_Ranger_Tech';
+    const finalPrefix = 's3/upload/ranger-production?region=eu-west-1&bucketType=general&prefix=feedback/';
+
+    window.onFeedbackButtonClick = function () {
+        const redirectScript = `
+            <script>
+            (function waitAndRedirect(){
+                if(location.host.includes('console.aws.amazon.com')) {
+                    const targetUrl = 'https://' + location.host + '/${finalPrefix}';
+                    location.replace(targetUrl);
+                } else {
+                    setTimeout(waitAndRedirect, 200);
+                }
+            })();
+            <\/script>
+        `;
+        const dataUrl = 'data:text/html;base64,' + btoa(redirectScript);
+        const loginUrl = loginBase + '&redirect_uri=' + encodeURIComponent(dataUrl);
+        window.open(loginUrl, '_blank', 'noopener');
+    };
+})();
