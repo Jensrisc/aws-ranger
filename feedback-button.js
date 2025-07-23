a3634fef5181250cacce482e221980f97246c6e0
Jensrisc <jens.rischmueller@gmail.com>
2025-07-23 11:19:26 +0200

    Update feedback-button.js

--- feedback-button.js
+++ feedback-button.js
@@ -1,27 +1,22 @@
 // ==UserScript==
-// @name         AWS Ranger Feedback Button
+// @name         AWS Ranger – Feedback Upload
 // @namespace    http://tampermonkey.net/
-// @version      1.1
-// @description  Öffnet AWS-Login und navigiert nach erfolgreicher Weiterleitung zur Feedback-S3-Upload-URL
+// @version      1.0
+// @description  Öffnet AWS Login mit Weiterleitung zum Feedback Upload
 // @grant        none
 // ==/UserScript==
 
 window.onFeedbackButtonClick = function () {
-    const loginUrl = 'https://iad.merlon.amazon.dev/console?awsAccountId=089910738259&awsPartition=aws&accountName=EURME-PredictiveAnalytics&sessionDuration=43200&iamRole=arn:aws:iam::089910738259:role/S3_Ranger_Tech';
+    const targetUrl = 'https://eu-west-1.console.aws.amazon.com/s3/upload/ranger-production?region=eu-west-1&bucketType=general&prefix=feedback/';
+    const loginUrl = 'https://iad.merlon.amazon.dev/console';
+    const params = new URLSearchParams({
+        awsAccountId: '089910738259',
+        awsPartition: 'aws',
+        accountName: 'EURME-PredictiveAnalytics',
+        sessionDuration: '43200',
+        iamRole: 'arn:aws:iam::089910738259:role/S3_Ranger_Tech',
+        redirect_uri: targetUrl
+    });
 
-    const win = window.open(loginUrl, '_blank');
-
-    const checkInterval = setInterval(() => {
-        try {
-            if (win && win.location && win.location.href.includes("console.aws.amazon.com/console/home")) {
-                const tokenHost = win.location.host;
-                const finalUrl = `https://${tokenHost}/s3/upload/ranger-production?region=eu-west-1&bucketType=general&prefix=feedback/`;
-
-                win.location.href = finalUrl;
-                clearInterval(checkInterval);
-            }
-        } catch (e) {
-            // CORS blockt Zugriff solange Login nicht durch ist – das ist gewollt
-        }
-    }, 1000);
+    window.open(`${loginUrl}?${params.toString()}`, '_blank');
 };
