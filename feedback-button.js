06de9339dc99e1215afaae24f1bdb36c1da019fc
Jensrisc <jens.rischmueller@gmail.com>
2025-07-23 11:14:36 +0200

    Update feedback-button.js

--- feedback-button.js
+++ feedback-button.js
@@ -1,18 +1,27 @@
 // ==UserScript==
 // @name         AWS Ranger Feedback Button
 // @namespace    http://tampermonkey.net/
-// @version      1.0
-// @description  Feedback Upload Button mit Login-Redirect
+// @version      1.1
+// @description  Öffnet AWS-Login und navigiert nach erfolgreicher Weiterleitung zur Feedback-S3-Upload-URL
 // @grant        none
 // ==/UserScript==
 
 window.onFeedbackButtonClick = function () {
-    const currentHost = window.location.host; // dynamischer Token-Host
-    const targetUrl = `https://${currentHost}/s3/upload/ranger-production?region=eu-west-1&bucketType=general&prefix=feedback/`;
+    const loginUrl = 'https://iad.merlon.amazon.dev/console?awsAccountId=089910738259&awsPartition=aws&accountName=EURME-PredictiveAnalytics&sessionDuration=43200&iamRole=arn:aws:iam::089910738259:role/S3_Ranger_Tech';
 
-    const loginBase = 'https://iad.merlon.amazon.dev/console?awsAccountId=089910738259&awsPartition=aws&accountName=EURME-PredictiveAnalytics&sessionDuration=43200&iamRole=arn:aws:iam::089910738259:role/S3_Ranger_Tech';
+    const win = window.open(loginUrl, '_blank');
 
-    const loginUrl = `${loginBase}&redirect_uri=${encodeURIComponent(targetUrl)}`;
+    const checkInterval = setInterval(() => {
+        try {
+            if (win && win.location && win.location.href.includes("console.aws.amazon.com/console/home")) {
+                const tokenHost = win.location.host;
+                const finalUrl = `https://${tokenHost}/s3/upload/ranger-production?region=eu-west-1&bucketType=general&prefix=feedback/`;
 
-    window.open(loginUrl, '_blank');
+                win.location.href = finalUrl;
+                clearInterval(checkInterval);
+            }
+        } catch (e) {
+            // CORS blockt Zugriff solange Login nicht durch ist – das ist gewollt
+        }
+    }, 1000);
 };
