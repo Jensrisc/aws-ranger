2444f9baa5bf7495a3f4c0251e4e19a7038372ff
Jensrisc <jens.rischmueller@gmail.com>
2025-07-23 10:42:41 +0200

    Update feedback-button.js

--- feedback-button.js
+++ feedback-button.js
@@ -1,7 +1,7 @@
 (function () {
     'use strict';
 
-    const loginBase = 'https://iad.merlon.amazon.dev/console?awsAccountId=089910738259&awsPartition=aws&accountName=EURME-PredictiveAnalytics&sessionDuration=43200&iamRole=arn:aws:iam::089910738259:role/S3_Ranger_Tech';
+    const loginBase = 'iad.merlon.amazon.dev/console?awsAccountId=089910738259&awsPartition=aws&accountName=EURME-PredictiveAnalytics&sessionDuration=43200&iamRole=arn:aws:iam::089910738259:role/S3_Ranger_Tech';
     const finalPrefix = 's3/upload/ranger-production?region=eu-west-1&bucketType=general&prefix=feedback/';
 
     window.onFeedbackButtonClick = function () {
