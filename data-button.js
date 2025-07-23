7ceabcd269f2dddc03d0fe1774acb907226a84dc
Jensrisc <jens.rischmueller@gmail.com>
2025-07-23 10:21:54 +0200

    Update data-button.js

--- data-button.js
+++ data-button.js
@@ -1,8 +1,9 @@
 window.DataButton = {
     onClick: () => {
         const loginBase = 'https://iad.merlon.amazon.dev/console?awsAccountId=089910738259&awsPartition=aws&accountName=EURME-PredictiveAnalytics&sessionDuration=43200&iamRole=arn:aws:iam::089910738259:role/S3_Ranger_Tech';
-        const target = 'https://eu-west-1.console.aws.amazon.com/s3/upload/ranger-production?region=eu-west-1&bucketType=general&prefix=data/';
-        const redirectLink = `${loginBase}&redirect_uri=${encodeURIComponent(target)}`;
-        window.open(redirectLink, '_blank');
+        const currentHost = window.location.host;
+        const targetUrl = `https://${currentHost}/s3/upload/ranger-production?region=eu-west-1&bucketType=general&prefix=data/`;
+        const finalUrl = `${loginBase}&redirect_uri=${encodeURIComponent(targetUrl)}`;
+        window.open(finalUrl, '_blank');
     }
 };
