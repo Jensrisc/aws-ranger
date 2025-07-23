b8d94256b5c10e65ccb403cb95bb8bb3cd1d40f1
Jensrisc <jens.rischmueller@gmail.com>
2025-07-23 11:04:29 +0200

    Update feedback-button.js

--- feedback-button.js
+++ feedback-button.js
@@ -1,24 +1,18 @@
-(function () {
-    'use strict';
+// ==UserScript==
+// @name         AWS Ranger Feedback Button
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @description  Feedback Upload Button mit Login-Redirect
+// @grant        none
+// ==/UserScript==
 
-    const loginBase = 'iad.merlon.amazon.dev/console?awsAccountId=089910738259&awsPartition=aws&accountName=EURME-PredictiveAnalytics&sessionDuration=43200&iamRole=arn:aws:iam::089910738259:role/S3_Ranger_Tech';
-    const finalPrefix = 's3/upload/ranger-production?region=eu-west-1&bucketType=general&prefix=feedback/';
+window.onFeedbackButtonClick = function () {
+    const currentHost = window.location.host; // dynamischer Token-Host
+    const targetUrl = `https://${currentHost}/s3/upload/ranger-production?region=eu-west-1&bucketType=general&prefix=feedback/`;
 
-    window.onFeedbackButtonClick = function () {
-        const redirectScript = `
-            <script>
-            (function waitAndRedirect(){
-                if(location.host.includes('console.aws.amazon.com')) {
-                    const targetUrl = 'https://' + location.host + '/${finalPrefix}';
-                    location.replace(targetUrl);
-                } else {
-                    setTimeout(waitAndRedirect, 200);
-                }
-            })();
-            <\/script>
-        `;
-        const dataUrl = 'data:text/html;base64,' + btoa(redirectScript);
-        const loginUrl = loginBase + '&redirect_uri=' + encodeURIComponent(dataUrl);
-        window.open(loginUrl, '_blank', 'noopener');
-    };
-})();
+    const loginBase = 'https://iad.merlon.amazon.dev/console?awsAccountId=089910738259&awsPartition=aws&accountName=EURME-PredictiveAnalytics&sessionDuration=43200&iamRole=arn:aws:iam::089910738259:role/S3_Ranger_Tech';
+
+    const loginUrl = `${loginBase}&redirect_uri=${encodeURIComponent(targetUrl)}`;
+
+    window.open(loginUrl, '_blank');
+};
